bb05929be995a9123d6f227d6de4da5d38361d15
Maartje <maartjeschutte@gmail.com>
2019-07-23 14:07:50 +0200

    Add method do stuff()

--- App.java
+++ App.java
@@ -2,4 +2,9 @@ class App {
     public static void main(String args[]) {
         System.out.println("App.main");
     }
+
+    public void doStuff(){
+        System.out.println("Feature x: do stuff");
+        
+    }
 }
